45dea23cd76c1d4e1fe12d9cb7a63d1d20178755
vkz@8200e/k-S9CD <116643083+Kusanagi8200@users.noreply.github.com>
2025-05-11 18:05:51 +0200

    Update ollama-wrapper.sh

--- KuzChat/ollama-wrapper.sh
+++ KuzChat/ollama-wrapper.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Forcer un environnement propre
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-export HOME=/root  # ou l’utilisateur qui a installé ollama
+export HOME=/root  
 
-# Exécuter ollama ps et supprimer les couleurs ANSI
+
 /usr/local/bin/ollama ps | sed -r 's/\x1B\[[0-9;]*[a-zA-Z]//g'
-
-#chmod +x /usr/local/bin/ollama-wrapper.sh
-#sudo -u www-data /usr/local/bin/ollama-wrapper.sh
 
 case "$1" in
     list)
